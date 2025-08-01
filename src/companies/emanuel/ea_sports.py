4e613f75d8a091614fb6d7a1f86d2168b81a1a4e
Emanuel-DevX <jefymoxi@gmail.com>
2025-08-01 16:10:48 -0700

    Define fetch_ea_jobs() function with id filter

---
+++ src/companies/emanuel/ea_sports.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_ea_jobs():
+    url = f'https://jobs.ea.com/en_US/careers/Home/?4538=%5B10259467%5D&4538_format=3021&listFilterMode=1&jobRecordsPerPage=100'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    articles = soup.find_all(id=lambda x: x and x.startswith("article--"))
+    articles = soup.find_all(id=lambda x: x and x.startswith("article--"))
+    for a in articles:
+        listing = [line.strip() for line in a.text.split()]
+        print(listing)
+
+
+fetch_ea_jobs()
